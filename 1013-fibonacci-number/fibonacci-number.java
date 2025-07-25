6634c2e445c1940153fc274f87289150ef5d221f
amitarya5822 <amitarya5822@gmail.com>
2025-07-26 00:08:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (37.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,29 +1,29 @@
-// class Solution {
+class Solution {
     // memoization(top-down)
-    
-//     public int fib(int n) {
-//         int dp[] = new int[n+1];
-//         Arrays.fill(dp,-1);
-//         return solve(n,dp);
-//     }
-//         public int solve(int n,int[] dp){
-//             if(n<= 1) return n;
-//             if(dp[n] != -1) return dp[n];
-//             dp[n] = solve(n-1,dp) + solve(n-2,dp);
-//             return dp[n];
-//         }
-//  }
+
+    public int fib(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+        public int solve(int n,int[] dp){
+            if(n<= 1) return n;
+            if(dp[n] != -1) return dp[n];
+            dp[n] = solve(n-1,dp) + solve(n-2,dp);
+            return dp[n];
+        }
+ }
 
 //  Bottom-Up (Tabulation)
-class Solution{
-    public int fib(int n){
-        if(n<=1) return n;
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
-    }
-}
+// class Solution{
+//     public int fib(int n){
+//         if(n<=1) return n;
+//         int[] dp = new int[n+1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i = 2;i<=n;i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
