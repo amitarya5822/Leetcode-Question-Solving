1b0362b2a1ad011c818b577a9fbb9ead22ab630c
amitarya5822 <amitarya5822@gmail.com>
2025-08-10 23:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (43.37%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,13 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-        char[] ch = s.toCharArray(); // convert String into char Array b/c array is mutable but String not
-        for(int i = 0;i<ch.length;i++){
-            if(ch[i] >= 'A' && ch[i] <= 'Z'){ //ASCII ranges from 65 to 90
-                ch[i] = (char) (ch[i]+32); // b/c ASCII of a is 97,diff.is 32
-
-            }
-        }
-        return new String(ch);
+        return s.toLowerCase();
     }
 }
 
