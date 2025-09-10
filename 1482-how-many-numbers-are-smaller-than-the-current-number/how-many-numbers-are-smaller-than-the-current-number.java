cf63c4bd9a52f23808ea3fd820fc6ee2c9efcc21
amitarya5822 <amitarya5822@gmail.com>
2025-09-10 22:22:50 +0530

    Time: 5 ms (85.78%) | Memory: 44.9 MB (13.46%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,17 +1,39 @@
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+//         int[] result = new int[nums.length];
+
+//         for(int i = 0;i< nums.length;i++){
+//             int count = 0;
+//           for(int j = 0;j<nums.length;j++){
+//             if(nums[j] < nums[i]){
+//                 count++;
+//             }
+//           }
+//           result[i] = count;
+//         }
+        
+//         return result;
+//     }
+// }
+
+// using hashmap
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] result = new int[nums.length];
-
-        for(int i = 0;i< nums.length;i++){
-            int count = 0;
-          for(int j = 0;j<nums.length;j++){
-            if(nums[j] < nums[i]){
-                count++;
-            }
-          }
-          result[i] = count;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] copy = nums.clone();
+        
+        Arrays.sort(copy);
+        
+        for (int i = 0; i < nums.length; i++) {
+            map.putIfAbsent(copy[i], i);
         }
         
-        return result;
+        for (int i = 0; i < nums.length; i++) {
+            copy[i] = map.get(nums[i]);
+        }
+        
+        return copy;
     }
 }
